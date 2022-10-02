573e0c38378c9af0621ab515aaa2ec571c05f6b7
nikitam1337 <nikitam.l@mail.ru>
2022-10-02 18:48:57 +0300

    Добавил 3-е задание

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите целое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+
+Console.Write(userNumber1 + " -> ");
+
+if (userNumber1 % 2 == 0) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
